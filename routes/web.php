155974f01d31defe3526761352b41e39a01b12d1
tbdev <52330541+tbdev@users.noreply.github.com>
2021-01-04 23:26:04 +0100

    Fix

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,1 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MonitorController;
-
-Route::get('monitors/{url}', [MonitorController::class, 'getSamplesByUrl'])->where('url', '.*');
